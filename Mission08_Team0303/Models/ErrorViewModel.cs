a448d2a6effff9af61fa962a3507f2c8f45c1e6e
David Murdock <davemurdock55@gmail.com>
2023-02-21 14:39:25 -0700

    Add project files.

---
+++ Mission08_Team0303/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_Team0303.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
